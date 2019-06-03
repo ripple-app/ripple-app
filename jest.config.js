35dbdfdacddb09959f4172fb26cfb541cb4f6486
myoungsubsim <rop2002@naver.com>
2019-06-03 16:39:11 +0900

    fix: jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     verbose: true,
     testEnvironment: 'node',
     testMatch: ['**/*.test.js'],
+    testPathIgnorePatterns: ['/node_modules/', '/environment/'],
     collectCoverage: true,
     coverageReporters: ['text', 'html', 'lcov'],
     coverageDirectory: 'coverage/unit'
